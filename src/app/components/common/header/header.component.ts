02446c9a8a39987d57329656a0f0e58a4e357571
keencho <seyoung050412@gmail.com>
2020-10-25 18:16:00 +0900

    responsive header(design)

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,20 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 })
 export class HeaderComponent implements OnInit {
 
+  // font-awesome 사용시 이런식으로 import 해와서 변수에 할당해줘야한다.
   faGithub = faGithub;
+  faEnvelope = faEnvelope;
+  faHome = faHome;
 
-  constructor() { }
+  goToURL(url): void {
+    window.open(url, '_blank');
+  }
+
+  sendEmail(): void {
+    window.open('mailto:seyoung050412@gmail.com');
+  }
+
+  constructor() {}
 
   ngOnInit(): void {
   }
