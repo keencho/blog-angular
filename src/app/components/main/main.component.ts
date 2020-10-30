10932c800fc96b2093f3e2181871d4c225de8a60
keencho <seyoung050412@gmail.com>
2020-10-30 18:12:21 +0900

    article card 완성

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,11 @@
 /* tslint:disable:no-trailing-whitespace */
 import {Component, HostListener, OnInit} from '@angular/core';
 import {isMobile} from '../../Utils/screen.utils';
+import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
+
+interface Articles {
+  article: string;
+}
 
 @Component({
   selector: 'app-main',
@@ -8,6 +13,22 @@ import {isMobile} from '../../Utils/screen.utils';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+
+  faAngleRight = faAngleRight;
+
+  articles: Articles[] = [
+    { article: '기사3' },
+    { article: '기사4' },
+    { article: '기사5' },
+    { article: '기사5' },
+    { article: '기사6' },
+    { article: '기사7' },
+    { article: '기사8' },
+    { article: '기사9' },
+    { article: '기사10' },
+  ];
+  
+  image = 'https://pfh.goodsflow.com/resources/image/2020-10-30/5755f651-79cb-4746-9f56-c564325665da.jpg';
 
   screenClass: string;
 
